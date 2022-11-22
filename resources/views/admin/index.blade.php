b85b6a2b6718e91b5005eded5c44a97999ad7903
JPampo <joaopedro.pamponet@gmail.com>
2022-11-22 11:36:34 -0300

    table secretaria

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,59 +6,51 @@
 <form action="/admin/create">
 <button type="submit" class="btn btn-primary" href="/admin/create">Adicionar dados</button>
 </form>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Cursos:</div>
-                    <div class="row my-3 justify-content-left">
-                        @foreach ($cursos as $curso)
-                            <p>Nome: {{ $curso->name }}</p>
-                        @endforeach
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach ($cursos as $curso)
+</p><tr>
+      <th scope="row">1</th>
+      <td><p>{{ $curso->name }}
+      <div class="mx-auto" style="width: 200px;">
+    <button type="button" class="btn btn-danger">Remover</button>
+    <button type="button" class="btn btn-success">Editar</button>
 </div>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Alunos:</div>
-                    <div class="row my-3 justify-content-left">
-                        @foreach ($users as $user)
-                            @if ($user->perm == 0)
-                            <p>Nome: {{ $user->name }}</p>
-                            @endif
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Professores:</div>
-                    <div class="row my-3 justify-content-left">
-                        @foreach ($users as $user)
-                            @if ($user->perm == 1)
-                                <p>Nome: {{ $user->name }}</p>
-                            @endif
-                        @endforeach
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    </td>
+      
+    </tr>
+@endforeach
+    
+  </tbody>
+</table>
+@foreach ($cursos as $curso)
+<p>{{ $curso->name }}</p>
+@endforeach
+             
 
 
+@foreach ($users as $user)
+@if ($user->perm == 0)
+<p>Nome: {{ $user->name }}</p>
+@endif
+@endforeach
+     
+
+
+@foreach ($users as $user)
+@if ($user->perm == 1)
+<p>Nome: {{ $user->name }}</p>
+ @endif
+ @endforeach
+                  
 
 @endsection
