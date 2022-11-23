904c5edec922549b53029108ea494fa35f970e4d
ArthrM <arthur.jsm@hotmail.com>
2022-11-22 22:17:12 -0300

    admin index revamp

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,54 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
-
-<p>Página do admin</p>
+<p style="font-size:35px;" class="ms-5">Página do admin</p>
 <form action="/admin/create">
-<button type="submit" class="btn btn-primary" href="/admin/create">Adicionar dados</button>
+<button type="submit" class="btn btn-primary ms-5" href="/admin/create">Adicionar dados</button>
 </form>
 
-<table class="table">
-  <thead>
+<table class="table container mt-3 table-hover">
+  <thead class="table-success">
     <tr>
-      <th scope="col">Código</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Deletar</th>
-      <th scope="col">Editar</th>
+      <th scope="col">ID</th>
+      <th scope="col">Curso</th>
+      <th scope="col">Ações</th>
     </tr>
   </thead>
   <tbody>
   @foreach ($cursos as $curso)
-</p><tr>
-      <th scope="row">1</th>
-      <td><p>{{ $curso->name }}</td>
-       <td> <button type="button" class="btn btn-danger">Remover</button></td>
-      <td><button type="button" class="btn btn-success">Editar</button></td>
+<tr>
+      <th scope="row">{{ $curso->id }}</th>
+      <td>{{ $curso->name }}</td>
+      <td>
+        <button type="button" class="btn btn-sm btn-info">
+            <img src="/images/info-icon.png" alt="Info" class="icons rounded mx-auto d-block">
+        </button> 
+        <button type="button" class="btn btn-sm btn-success">
+            <img src="/images/edit-icon.png" alt="Editar" class="icons rounded mx-auto d-block">
+        </button>
+        <button type="button" class="btn btn-sm btn-danger">
+            <img src="/images/bin-icon.png" alt="Remover" class="icons rounded mx-auto d-block">
+        </button>
+      </td>
     
       
     </tr>
-@endforeach
+    @endforeach
     
   </tbody>
-</table>
-@foreach ($cursos as $curso)
-<p>{{ $curso->name }}</p>
-@endforeach
-             
 
+  <thead class="table-primary">
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Professor</th>
+            <th scope="col">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach ($users as $user)
+    @if ($user->perm == 1)
+    <tr>
+        <th scope="row">{{ $user->id }}</th>
+        <td>{{ $user->name }}</td>
+        <td>
+            <button type="button" class="btn btn-sm btn-info">
+                <img src="/images/info-icon.png" alt="Info" class="icons rounded mx-auto d-block">
+            </button>
+            <button type="button" class="btn btn-sm btn-success">
+                <img src="/images/edit-icon.png" alt="Editar" class="icons rounded mx-auto d-block">
+            </button>
+            <button type="button" class="btn btn-sm btn-danger">
+                <img src="/images/bin-icon.png" alt="Remover" class="icons rounded mx-auto d-block">
+            </button>
+            
+        </td>
 
-@foreach ($users as $user)
-@if ($user->perm == 0)
-<p>Nome: {{ $user->name }}</p>
-@endif
-@endforeach
+    </tr>
+    @endif
+    @endforeach
+    </tbody> 
+
+  <thead class="table-warning">
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Aluno</th>
+            <th scope="col">Ações</th>
+        </tr>
+  </thead>
+    <tbody>
+    @foreach ($users as $user)
+    @if ($user->perm == 0)
+    <tr>
+        <th scope="row">{{ $user->id }}</th>
+        <td>{{ $user->name }}</td>
+        <td>
+            <button type="button" class="btn btn-sm btn-info">
+                <img src="/images/info-icon.png" alt="Info" class="icons rounded mx-auto d-block">
+            </button>
+            <button type="button" class="btn btn-sm btn-success">
+                <img src="/images/edit-icon.png" alt="Editar" class="icons rounded mx-auto d-block">
+            </button>
+            <button type="button" class="btn btn-sm btn-danger">
+                <img src="/images/bin-icon.png" alt="Remover" class="icons rounded mx-auto d-block ">
+            </button>
+            
+        </td>
+
+    </tr>
+    @endif
+    @endforeach
+    </tbody>
      
-
-
-@foreach ($users as $user)
-@if ($user->perm == 1)
-<p>Nome: {{ $user->name }}</p>
- @endif
- @endforeach
-                  
-
+</table>
+    
+     
 @endsection
